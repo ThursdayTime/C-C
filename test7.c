e7dfec12616ef60e730c79781fc904e56445c43f
qirong <qirong.zhang@outlook.com>
2015-08-08 06:29:38 -0400

    first C++

---
+++ test7.c
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int a[5],i=0,j=0,b[5],sum=0;
+	for(i=0;i<5;i++){
+		
+		cin>>a[i];
+		
+         	 }
+	for(i=0;i<5;i++){
+		if (a[i]<5){
+			cout<<a[i]<<endl;
+			b[j]=a[i];
+			j++;
+
+
+			
+			}
+		
+		}
+	for (i=0;i<j;i++){
+		sum+=b[i];
+		}
+	cout<<sum<<endl;
+	
+	return 0;
+}
